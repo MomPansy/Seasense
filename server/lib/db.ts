380e0312d6c60a8616f3078c6eceddb809287291
Jun Kai Tan <jax.tanjunkai@gmail.com>
2025-11-12 11:46:33 +0800

    feat: add readonly db conn for llm

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -3,10 +3,17 @@ import postgres from "postgres";
 import * as drizzleSchema from "server/drizzle/_index.ts";
 import { appEnvVariables } from "server/env.ts";
 
+// Main DB (Full CRUD)
 const connectionString = appEnvVariables.DB_URL;
 export const client = postgres(connectionString, { prepare: false });
-
 export const db = drizzle(client, { schema: drizzleSchema });
+
+// Read-only DB
+const readOnlyConnectionString = appEnvVariables.DB_READONLY_URL;
+export const readOnlyClient = postgres(readOnlyConnectionString, {
+  prepare: false,
+});
+export const readOnlyDb = drizzle(readOnlyClient, { schema: drizzleSchema });
 
 export type PostgresOptions = postgres.Options<
   Record<string, postgres.PostgresType>
--- server/zod/env.ts
+++ server/zod/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const appEnvVariablesSchema = z.object({
   DB_URL: z.string(),
+  DB_READONLY_URL: z.string(),
   ANTHROPIC_API_KEY: z.string(),
   MDH_API_KEY: z.string(),
   SEASENSE_USERNAME: z.string(),
