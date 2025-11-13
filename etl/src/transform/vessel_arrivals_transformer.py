b7ede3dc3572b48af865f4c09629b341f36d52af
Jun Kai Tan <jax.tanjunkai@gmail.com>
2025-11-13 17:04:55 +0800

    feat: etl add more logging for transformation details

--- etl/src/transform/vessel_arrivals_transformer.py
+++ etl/src/transform/vessel_arrivals_transformer.py
@@ -1,6 +1,7 @@
 import pytz
 from datetime import datetime
 from etl.src.transform import MdhDataTransformer
+from loguru import logger
 
 class VesselArrivalsTransformer(MdhDataTransformer):
     def __init__(self, conn, data_name, location_code_mappings):
@@ -8,6 +9,10 @@ class VesselArrivalsTransformer(MdhDataTransformer):
 
     def staging_transform_row(self, fetched_at, response_json):
         rows = []
+        location_from_transformed_count = 0
+        location_to_transformed_count = 0
+        logger.debug(f"Transforming {len(response_json)} records...")
+        
         if not response_json:
             return rows
         for record in response_json:
@@ -26,9 +31,11 @@ class VesselArrivalsTransformer(MdhDataTransformer):
             location_from = record["locationFrom"]
             if (location_from in self.location_code_mappings):
                 location_from = self.location_code_mappings.get(location_from)
+                location_from_transformed_count += 1
             location_to = record["locationTo"]
             if (location_to in self.location_code_mappings):
                 location_to = self.location_code_mappings.get(location_to)
+                location_to_transformed_count += 1
 
             rows.append({
                 "vessel_name": vessel_particulars["vesselName"],
@@ -40,4 +47,8 @@ class VesselArrivalsTransformer(MdhDataTransformer):
                 "location_to": location_to,
                 "fetched_at": fetched_at,
             })
+        
+        logger.debug(f"Shortened {location_from_transformed_count} long names to short names for 'location_from'.")
+        logger.debug(f"Shortened {location_to_transformed_count} long names to short names for 'location_to'.")
+            
         return rows
